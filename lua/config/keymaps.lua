950a55bf582213a066304fa7a1de3eeb9d2c3e38
Vraj Barot <vraj.barot@vetchip.com.au>
2025-05-09 16:55:22 +0800

    Personal lazygit config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>qq", ":q<CR>", { desc = "Quit", silent = true })
+keymap.set("n", "<leader>ww", ":w<CR>", { desc = "Save File", silent = true })
+keymap.set("n", "<leader>wq", ":wq<CR>", { desc = "Save and Quit", silent = true })
+keymap.set("i", "jk", "<Esc>", { noremap = true })
+
+-- Buffers
+keymap.set("n", "<Tab>", vim.cmd.bp, { desc = "Next Buffer", silent = true })
