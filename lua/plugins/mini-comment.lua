f504bb4bd886c87eb9ebf4f8bf9f9b01c15466e3
Colin Lear <colin.lear@vetchip.com.au>
2025-08-19 15:24:32 +0800

    Remap leader+/ to toggle comment

--- lua/plugins/mini-comment.lua
+++ lua/plugins/mini-comment.lua
@@ -1,11 +1,10 @@
 return {
   "echasnovski/mini.comment",
   version = false, -- use latest
-  config = function()
-    require("mini.comment").setup()
-
-    -- Remap <leader>/ to toggle comment
-    vim.keymap.set("n", "<leader>/", "gcc", { desc = "Toggle Comment", remap = true })
-    vim.keymap.set("v", "<leader>/", "gc", { desc = "Toggle Comment", remap = true })
-  end,
+  opts = {
+    mappings = {
+      comment_line = "<leader>/",
+      comment_visual = "<leader>/",
+    },
+  },
 }
