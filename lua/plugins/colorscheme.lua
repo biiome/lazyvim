b24e971d58e74bb868c3a494237db2bd9bf33d3a
Colin Lear <colin.lear@vetchip.com.au>
2025-08-19 09:44:02 +0800

    added multile configs, return whenever theme you want to use

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,4 @@
-return {
+local tokyonight = {
   "folke/tokyonight.nvim",
   opts = {
     transparent = { true },
@@ -8,3 +8,25 @@ return {
     },
   },
 }
+local catppuccin = {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+        term_colors = true,
+        float = {
+          transparent = true,
+          solid = false,
+        },
+      })
+
+      vim.cmd([[colorscheme catppuccin]])
+    end,
+  },
+}
+
+return { tokyonight }
