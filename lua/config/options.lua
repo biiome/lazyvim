98413414b41f0d771df03049ea55f810c574f575
Vraj Barot <vraj.barot@vetchip.com.au>
2026-03-31 10:33:09 +0800

    random other changes. dw about it

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Make nvm's node available to Mason / LSPs
+local node_path = vim.fn.expand("$HOME/.nvm/versions/node")
+-- Find the "default" alias or just grab the active one
+local default_node = vim.fn.system("ls -t " .. node_path .. " | head -1"):gsub("\n", "")
+if default_node ~= "" then
+  vim.env.PATH = node_path .. "/" .. default_node .. "/bin:" .. vim.env.PATH
+end
